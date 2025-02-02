e9ea81a3c74d30b16b777ad4dce976d6245abbc6
Upinder Kaur <kaurupinder28@gmail.com>
2025-02-02 19:01:14 +0530

    Add files via upload

---
+++ Day-1.cpp
@@ -0,0 +1,31 @@
+// FROM VERY SIMPLE AND BASIC QUESTIONS :- 
+
+// PRODUCT OF 2 NUMBERS :-
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a = 2;
+//     int b = 3;
+//     int c = a * b;
+//     cout << c << endl;
+// }
+
+// SUBTRACT OF 2 NUMBERS :-
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a = 3;
+//     int b = 2;
+//     int c = a - b;
+//     cout << c << endl;
+// }
+
+// PRINT THE NUMBER AS IT IS :-
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "6 * 3" << endl;
+}
