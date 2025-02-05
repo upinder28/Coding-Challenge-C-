42626ea8aab174b3a86a420401ef18e829ad2ae9
Upinder Kaur <kaurupinder28@gmail.com>
2025-02-05 21:46:52 +0530

    Add files via upload

---
+++ Day-4.cpp
@@ -0,0 +1,51 @@
+// PRINT THE PATTERN OF "4" :-
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for (int i = 1; i <= 5; i++) {
+//         for (int j = 1; j <= 6; j++) {
+//             cout << "4" << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// PRINT THE PATTERN :-
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for (int i = 1; i <= 5; i++) {
+//         for (int j = 1; j <= 5; j++) {
+//             cout << j * j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// PRINT THE PATTERN :- 
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     for (int i = 1; i <= 5; i++) {
+//         for (int j = 1; j <= 6; j++) {
+//             cout << j * j * j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// PRINT THE PATTERN :-
+
+#include<iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; i++){
+        for (char j = 'F'; j <= 'K'; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
